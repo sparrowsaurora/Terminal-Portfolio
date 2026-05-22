d58a78479b78c753fee78cbf63902068d94404ac
Sparrows <35826584+sparrowsaurora@users.noreply.github.com>
2026-05-22 23:17:26 +0800

    updated projects to include nocturne

--- js/commands.js
+++ js/commands.js
@@ -14,22 +14,22 @@ const commands = {
 about: /*html*/`Hi! I'm Ryan,
 I'm a software engineering student and content creator, who builds cool projects like this one.
 I also make YouTube videos about software engineering in my free time.
-I've just completed my Diploma of Information Technology (Advanced Programming) at North Metropolitan TAFE and 
-I'm passionate about building and learning new things.
+I've just completed my Diploma of Information Technology (Advanced Programming) at North Metropolitan TAFE 
+and I'm currently studying Programming doing my bachelor's degree at Curtin University.
 
 My fields of interest are:
 - Embedded systems
 - Automation
-- back-end development
+- Low-level Programming
 
 Currently working on exciting projects.
 (These are all commands, try it to find out more)
 - Bash-Salt - A small dev toolkit
-- Realtime Chat app - A TCP based ternimal chatroom
-- Terminal - A terminal-style portfolio
+- Realtime Chat app - A TCP based terminal chatroom
+- Nocturne - A TUI music app
 etc. (Type <span class="highlight">'projects'</span> to see more)
 
-I primarily use Python and C++ when building my projects.
+I primarily use C++ and C when building my projects.
 You can learn more about the projects that I have built using the <span class="highlight">'projects'</span> command.
 You can browse around my website to find out more about me.
 *Try using the <span class="highlight">'socials'</span> command.
@@ -64,9 +64,9 @@ sudo: `You're not an admin sry`,
 //     - flutter`,
 
     languages: `My Languages:
-    - Python        - C++
+    - C++           - C
     - Shell (bash)  - Rust 
-    - C             - PHP
+    - Python        - PHP
     - SQL & NoSQL
 Frameworks:
     - Flask
@@ -101,10 +101,15 @@ Frameworks:
     - Built with: C++
     - Link: <a href="https://github.com/sparrowsaurora/Realtime-chat-app">github.com/sparrowsaurora/Realtime-chat-app</a>
 
-# Terminal portfolio
-    - A <span class="highlight">'terminal'</span> style portfolio
-    - Built with: HTML, CSS, JavaScript
+# Nocturne
+    - A local TUI music player called<span class="highlight">'nocturne'</span> 
+    - Built with: C++
     - Link: <a href="https://github.com/sparrowsaurora/Terminal-Portfolio">github.com/sparrowsaurora/Terminal-Portfolio</a>`,
+
+// # Terminal portfolio
+//     - A <span class="highlight">'terminal'</span> style portfolio
+//     - Built with: HTML, CSS, JavaScript
+//     - Link: <a href="https://github.com/sparrowsaurora/Terminal-Portfolio">github.com/sparrowsaurora/Terminal-Portfolio</a>`,
 
 
     bashsalt: `A current free SaaP (Software as a Product) project im working on to help developers work more efficiently.
@@ -120,9 +125,14 @@ Made with HTML, minimal CSS and a LOT of JavaScript.`,
 // This project was my first real look into embedded systems and programming in C++ and it's built on a raspberry pi pico (rp2040).
 // It's a fun little project to practice low-level programming and working with embedded systems.`,
 
-    chatroom: `A real-time text chat application to help me better understand networking.
-A TCP connected chatroom with a monolithic architecture, allowing users to run a server, or join a room from the same base command using CLI arguments.
-It also has user customisations, like colours for a specific user's messages for clarity.`,
+//     chatroom: `A real-time text chat application to help me better understand networking.
+// A TCP connected chatroom with a monolithic architecture, allowing users to run a server, or join a room from the same base command using CLI arguments.
+// It also has user customisations, like colours for a specific user's messages for clarity.`,
+
+    nocturne: `A local TUI music app made because I am sick of spotify restricting the songs I can play.
+An configurable terminal-based app that interfaces with local audio files to play and control music (or other audio files).
+It was made to accompany an Embedded systems project which functions as a physical controller for this app.`,
+
 
     // welcome: /*html*/`<span class="header">Welcome! to my interactive web terminal portfolio.<br />For a list of available commands, type <span class="highlight">'help'</span>.</span>`
     welcome: /*html*/`<span class="header">If you've met me in person, this is the portfolio site I mentioned.<br />For a list of available commands, type <span class="highlight">'help'</span>.</span>`
